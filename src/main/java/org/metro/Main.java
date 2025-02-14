0f23899f8395cf1a3248e15265e0e6ed44399850
L0ngg <hohoanglong2508@gmail.com>
2025-02-14 20:48:41 +0700

    Test thu Material UI Swing

--- src/main/java/org/metro/Main.java
+++ src/main/java/org/metro/Main.java
@@ -1,6 +1,9 @@
 package org.metro;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import mdlaf.MaterialLookAndFeel;
 
 import org.metro.view.LoginFrame;
 
@@ -8,6 +11,13 @@ public class Main {
     public static void main(String[] args) {
         // Đặt encoding toàn cục cho ứng dụng
         System.setProperty("file.encoding", "UTF-8");
+
+        try {
+            // Thiết lập Material Look and Feel
+            UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
